6d579efe609bc8ac64ab51a5e9664661d2ace926
nccoutinho <natscc@student.ubc.ca>
2023-11-27 19:18:21 -0800

    Project and Task attributes

--- projecttracker/management/operations.py
+++ projecttracker/management/operations.py
@@ -16,12 +16,33 @@ class Operations:
         self.tasks[new_task.taskID] = new_task
         print(f"Task '{new_task.taskID}' added.")
         return new_task
-    
 
 class Project(Operations):
-
-
-
+    project_counter = 0
+    def __init__(self, Name, Priority, Duration, Comments, assignedTo, startDate, Deadline, Owner):
+        Project.project_counter += 1
+        self.projectID = f'P{Project.project_counter:04}'
+        self.projectName = Name
+        self.projectPriority = Priority
+        self.projectDuration = Duration
+        self.projectComments = Comments
+        self.assignedToProjectTL = assignedTo
+        self.projectStartDate = startDate 
+        self.projectDeadline = Deadline
+        self.projectOwner = Owner
+        self.IsProjectCompleted = 'N'
 
 
 class Task(Project):
+    task_counter = 0
+    def __init__(self, Name, Priority, Duration, Comments, assignedTo, startDate, Deadline):
+        Task.task_counter += 1
+        self.taskID = f'T{Task.task_counter:04}'
+        self.taskName = Name
+        self.taskPriority = Priority
+        self.taskDuration = Duration
+        self.taskComments = Comments
+        self.assignedToTask = assignedTo
+        self.taskStartDate = startDate
+        self.taskDeadline = Deadline
+        self.IsTaskCompleted = 'N'
