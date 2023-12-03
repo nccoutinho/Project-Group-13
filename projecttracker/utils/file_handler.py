1edd79231e5fbecf4ff396cd5548628f5d5dedb9
nccoutinho <natscc@student.ubc.ca>
2023-12-02 17:30:27 -0800

    file_handler docstring

--- projecttracker/utils/file_handler.py
+++ projecttracker/utils/file_handler.py
@@ -2,11 +2,31 @@ import json
 import pandas as pd
 
 def write_to_json(obj, file_name, method = 'a'):
+    '''
+    Writes object data into a JSON file.
+
+    Args:
+        obj: Object to be written to JSON.
+        file_name (str): Name of the JSON file.
+        method (str): Writing mode, default is 'a' (append).
+
+    Returns:
+        None
+    '''
     with open(file_name, method) as json_file:
         json.dump(obj.__dict__, json_file)
         json_file.write('\n')
 
 def read_from_json(file_name):
+    '''
+    Reads data from a JSON file.
+
+    Args:
+        file_name (str): Name of the JSON file.
+
+    Returns:
+        list: List containing the read JSON data.
+    '''
     result_list = []
     try:
         with open(file_name, 'r') as json_file:
@@ -18,6 +38,15 @@ def read_from_json(file_name):
 
   
 def delete_all_objects(file_path):
+    '''
+    Deletes all content from a specified file.
+
+    Args:
+        file_path (str): Path to the file to be cleared.
+
+    Returns:
+        None
+    '''
     try:
         # Open the JSON file and read its content line by line
         with open(file_path, 'r') as file:
@@ -31,10 +60,31 @@ def delete_all_objects(file_path):
         print(f"File '{file_path}' not found.")
 
 def write_to_json_dict(obj, file_name, method = 'a'):
+    '''
+    Writes object data into a JSON file.
+
+    Args:
+        obj: Object to be written to JSON.
+        file_name (str): Name of the JSON file.
+        method (str): Writing mode, default is 'a' (append).
+
+    Returns:
+        None
+    '''
     with open(file_name, method) as json_file:
         json.dump(obj, json_file)
         json_file.write('\n')
 
 def download_csv(data, file_path):
+    '''
+    Downloads a Pandas DataFrame as a CSV file.
+
+    Args:
+        data: Pandas DataFrame to be downloaded as CSV.
+        file_path (str): Path to save the CSV file.
+
+    Returns:
+        None
+    '''
     file_path = f"{file_path}/project_details.csv"
     data.to_csv(file_path, index=False)
